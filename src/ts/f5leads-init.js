f8cef5d90e67ecd410e5bb447b87eda69f05feda
Belov Roman <rnbelov96@gmail.com>
2021-10-26 14:53:43 +0300

    Fix lead email

--- src/ts/f5leads-init.js
+++ src/ts/f5leads-init.js
@@ -1,6 +1,5 @@
 document.f5leads = {};
-// document.f5leads.emails = 'artcraft_coworking@mail.ru'; // comma separated
-document.f5leads.emails = 'belov.roh777@yandex.ru';
+document.f5leads.emails = 'artcraft_coworking@mail.ru, leads@conversionpro.agency'; // comma separated
 document.f5leads.send_to_bitrix = 'false';
 document.f5leads.expect_second_form = '0';
 document.f5leads.counterId = '';
